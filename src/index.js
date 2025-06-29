62aac5f785bce993eb7b2cc5d8c8a3345ba8597e
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-06-29 14:30:36 +0200

    Made adjustment to the context

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function generateIdea(event) {
     let userInput = document.querySelector("#user-input").value;
 
     let apiKey = "2c13e0a2b6fe347b0421bb02eef2o43t";
-    let context = "You are a startup idea generator. You have knowledge of different business concepts and ideas. You provide unique business ideas and key features. Please provide you answer in html for example: (<h3> Generated business idea title</h3><li>List three business ideas</li><p>A short description of the business cocepts in four lines</p>. Please sign SheCodes AI at the bottom left inside a strong html tag ";
+    let context = "You are a startup idea generator. You have knowledge of different business concepts and ideas. You provide unique business ideas and key features. Please provide you answer in html for example: (<h3> Generated business idea title</h3><li>List three business ideas</li><p>A short description of the business cocepts in four lines</p>. Please sign SheCodes AI at the bottom left inside a strong html tag. Please never include ```html in the display ";
     let prompt = `Please provide me a business idea about ${userInput}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
